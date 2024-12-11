823dc42be42268310cad882c0708b3eaba37a3a5
hausnes <jb.hausnes@gmail.com>
2024-12-11 19:02:03 +0100

    Fjerner plassholdertekst ved første run

--- kode.js
+++ kode.js
@@ -1,12 +1,20 @@
+let first = true; // For å sjekke om det allerede ligg nokon i lista til å begynne med
+
 function lagOgLeggTilListeElement(elev) {
-    let tid = new Date();
-    let listeElement = document.createElement('li');
-    listeElement.innerHTML = '<b>' + elev + '</b> (' + tid.toLocaleTimeString() + ')';
-    document.getElementById("liste").appendChild(listeElement);
+    let tid = new Date(); // Hent ut tida no
+    let listeElement = document.createElement('li'); // Lag eit nytt liste-element
+    listeElement.innerHTML = '<b>' + elev + '</b> (' + tid.toLocaleTimeString() + ')'; // Set innhaldet i liste-elementet
+    document.getElementById("liste").appendChild(listeElement); // Legg til liste-elementet i HTML-lista
 }
 
 document.getElementById("skjema").addEventListener("submit", function (evt) {
-    evt.preventDefault();
-    let elev = document.getElementById("navn").value;
-    lagOgLeggTilListeElement(elev);
+    evt.preventDefault(); // SPA - Single Page Application
+
+    if (first) { // Første gangen me legg til ein elev, fjern plassholdertekst
+        document.getElementById("liste").innerHTML = "";
+        first = false;
+    }
+
+    let elev = document.getElementById("navn").value; // Hent ut verdien frå input-feltet
+    lagOgLeggTilListeElement(elev); // Lag og legg til eit nytt liste-element
 });
