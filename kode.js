d321dd0c6a9d73a8246ab7e3b531425b01db7342
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-12-12 15:55:03 +0100

    Lagt til tidsbruk frå range-input

--- kode.js
+++ kode.js
@@ -1,9 +1,12 @@
 let first = true; // For å sjekke om det allerede ligg nokon i lista til å begynne med
 
-function lagOgLeggTilListeElement(elev) {
+function lagOgLeggTilListeElement(elev, tidsbruk) {
     let tid = new Date(); // Hent ut tida no
     let listeElement = document.createElement('li'); // Lag eit nytt liste-element
     listeElement.innerHTML = '<b>' + elev + '</b> (' + tid.toLocaleTimeString() + ')'; // Set innhaldet i liste-elementet
+    if (tidsbruk==2) { // Sjekk om tidsbruk er oppgitt
+        listeElement.style.color = "red"; // Set farge på teksten i liste-elementet
+    }
     document.getElementById("liste").appendChild(listeElement); // Legg til liste-elementet i HTML-lista
 }
 
@@ -16,5 +19,7 @@ document.getElementById("skjema").addEventListener("submit", function (evt) {
     }
 
     let elev = document.getElementById("navn").value; // Hent ut verdien frå input-feltet
-    lagOgLeggTilListeElement(elev); // Lag og legg til eit nytt liste-element
+    document.getElementById("navn").value = ""; // Nullstill input-feltet
+    let tidsbruk = document.getElementById("tidsbruk").value; // Hent ut verdien frå input-feltet
+    lagOgLeggTilListeElement(elev, tidsbruk); // Lag og legg til eit nytt liste-element
 });
